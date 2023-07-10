599472e06aeb0f64beefac2942cd000ff0333cf2
deeksha-varma <deekshav72@gmail.com>
2023-07-10 23:09:41 +0200

    Add username form input styles and js validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log('Connected!');
+// Code for form validation referenced from https://www.freecodecamp.org/news/form-validation-with-html5-and-javascript/
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", validate);
+
+function validate(e) {
+    e.preventDefault();
+
+    const userNameField = document.getElementById("username");
+
+    if (!userNameField.value) {
+        const nameError = document.getElementById("usernameError");
+        nameError.classList.add("visible");
+        userNameField.classList.add("invalid");
+        nameError.setAttribute("aria-hidden", false);
+        nameError.setAttribute("aria-invalid", true);
+        nameError.innerText = "Please fill in a username to proceed!"
+    }
+}
